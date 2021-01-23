9459c7d8cab36d6b20eaa8463eedeed5a35211ca
Prajwal S R <prajwal.1rn18cs412@gmail.com>
2021-01-23 16:53:29 +0530

    first commit

---
+++ 7th program/7th program.py
@@ -0,0 +1,44 @@
+import numpy as np
+from urllib.request import urlopen
+import urllib
+import matplotlib.pyplot as plt # Visuals
+import seaborn as sns 
+import sklearn as skl
+import pandas as pd
+import pgmpy
+import sys
+
+Cleveland_data_URL = 'http://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.hungarian.data'
+np.set_printoptions(threshold=sys.maxsize)
+names = ['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal', 'heartdisease']
+heartDisease = pd.read_csv(urlopen(Cleveland_data_URL), names = names) 
+heartDisease.head()
+print(heartDisease.head())
+del heartDisease['ca']
+del heartDisease['slope']
+del heartDisease['thal']
+del heartDisease['oldpeak']
+heartDisease = heartDisease.replace('?', np.nan)
+heartDisease.dtypes
+print(heartDisease.dtypes)
+heartDisease.columns
+from pgmpy.models import BayesianModel
+from pgmpy.estimators import MaximumLikelihoodEstimator, BayesianEstimator
+model = BayesianModel([('age', 'trestbps'), ('age', 'fbs'), ('sex', 'trestbps'), ('sex', 'trestbps'), 
+                       ('exang', 'trestbps'),('trestbps','heartdisease'),('fbs','heartdisease'),
+                      ('heartdisease','restecg'),('heartdisease','thalach'),('heartdisease','chol')])
+
+model.fit(heartDisease, estimator=MaximumLikelihoodEstimator)
+
+print(model.get_cpds('age'))
+print(model.get_cpds('chol'))
+print(model.get_cpds('sex'))
+model.get_independencies()
+
+from pgmpy.inference import VariableElimination
+HeartDisease_infer = VariableElimination(model)
+
+q = HeartDisease_infer.query(variables=['heartdisease'], evidence={'age': 28},joint=False)
+print(q['heartdisease'])
+q = HeartDisease_infer.query(variables=['heartdisease'], evidence={'chol': 100},joint=False)
+print(q['heartdisease'])
